1f5505f64c21c7094aa0b332c91a532465acbbf8
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2025-03-13 10:55:51 +0800

    change codes in activity 2

--- source-codes/activity-2/client.py
+++ source-codes/activity-2/client.py
@@ -1,18 +1,17 @@
 import socket
 
-# Create a TCP socket for the client
+# Define client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server at localhost on port 8080
-client_socket.connect(('localhost', 8080))
+# Connect to the server
+client_socket.connect(('localhost', 8080))  # Relation: communicatesWith(ServerService)
 
-# Send a message to the server
-message = "Hello from Client!"
-client_socket.send(message.encode())
+# Send message
+client_socket.send("Hello from Client!".encode())
 
-# Receive response from server
+# Receive response
 response = client_socket.recv(1024).decode()
-print(f"Received from server: {response}")
+print("Message from server:", response)
 
 # Close the connection
 client_socket.close()
--- source-codes/activity-2/server.py
+++ source-codes/activity-2/server.py
@@ -1,27 +1,23 @@
 import socket
 
-# Create a TCP socket for the server
+# Define server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the server to localhost on port 8080
-server_socket.bind(('localhost', 8080))
-
-# Listen for incoming connections (max 1 client in this case)
+server_socket.bind(('localhost', 8080))  # Attribute: port = 8080
 server_socket.listen(1)
-print("Server is listening on port 8080...")
 
-# Accept a connection from the client
+print("Server is ready and listening on port 8080...")
+
+# Accept client connection
 client_socket, client_address = server_socket.accept()
-print(f"Connection established with client at {client_address}")
+print(f"Client connected from: {client_address}")
 
-# Receive data from the client
-data = client_socket.recv(1024).decode()
-print(f"Received from client: {data}")
+# Receive data from client
+message = client_socket.recv(1024).decode()
+print("Message from client:", message)
 
-# Send a response back to the client
-response = "Hello from Server!"
-client_socket.send(response.encode())
+# Send response
+client_socket.send("Hello from Server!".encode())
 
-# Close the connection
+# Close sockets
 client_socket.close()
 server_socket.close()
